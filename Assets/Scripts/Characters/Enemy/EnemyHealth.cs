ff6432c9704aed7cbc7cc018cc22a4893638b5ae
Sim_out <alik2006sootkill@gmail.com>
2024-03-24 18:07:33 +0700

    update

--- Assets/Scripts/Characters/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Characters/Enemy/EnemyHealth.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyHealth : MonoBehaviour,IDamageable , IEnemyShield, IItem
 {
+    [Header("Settings")]
     [SerializeField] private float MaxEnemyHealth = 2;
+    [SerializeField] private int ChanceDropItem = 1;
+    [SerializeField] private int ItemCount = 1;
+    [Header("GameObject")]
     [SerializeField] private GameObject ShieldGameObject;
     [HideInInspector]
     public float Health;
@@ -21,9 +26,7 @@ public class EnemyHealth : MonoBehaviour,IDamageable , IEnemyShield, IItem
         Health -= damageValue;
         if (Health <= 0)
         {
-            ScoreOnScene.OnGetScore.Invoke(DataBase.GetItemByID(ID).Score);
-            EventManager.OnObjectDestroy.Invoke();
-            Destroy(gameObject);
+            StartCoroutine(DeathDelay());
         }
         ShieldGameObject.SetActive(false);
     }
@@ -37,5 +40,18 @@ public class EnemyHealth : MonoBehaviour,IDamageable , IEnemyShield, IItem
     {
         yield return new WaitForSeconds(0.01f);
         Health+= health;
+    }
+    private IEnumerator DeathDelay()
+    {
+        System.Random rnd = new System.Random();
+        if (rnd.Next(0, ChanceDropItem) == 0)
+        {
+            Instantiate(DataBase.GetItemByID(rnd.Next(5, 5 + ItemCount).ToString()).ItemOnGround, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+        }
+        ScoreOnScene.OnGetScore.Invoke(DataBase.GetItemByID(ID).Score);
+        EventManager.OnObjectDestroy.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        Destroy(gameObject);
     }
 }
