c358519623ee57ca6e7fd3de86c57d91b7e0827e
Sim_out <alik2006sootkill@gmail.com>
2024-03-29 18:46:55 +0700

    update

--- Assets/Scripts/Menu/PlayerHud.cs
+++ Assets/Scripts/Menu/PlayerHud.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHud : MonoBehaviour
 {
     [SerializeField] private GameObject LoseMenu;
     [SerializeField] private GameObject WinMenu;
+    [SerializeField] private GameObject Menu;
+    [SerializeField] private Slider Slider;
     private void Awake()
     {
         EventManager.OnGameLose.AddListener(HandleGameLose);
@@ -38,5 +41,17 @@ public class PlayerHud : MonoBehaviour
         DOTween.KillAll();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1f;
+    }
+    public void MenuButton()
+    {
+        Menu.SetActive(!Menu.active);
+    }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void VolumeSlider()
+    {
+        AudioListener.volume = Slider.value;
     }
 }
