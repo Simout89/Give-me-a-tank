90141da0927d5370578898d92c0a3d177047f085
Sim_out <alik2006sootkill@gmail.com>
2024-03-29 23:38:44 +0700

    update

--- Assets/Scripts/Menu/PlayerHud.cs
+++ Assets/Scripts/Menu/PlayerHud.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
