a725ad2dfecad421b5ea0c4d85074b0ef3aef918
Sim_out <alik2006sootkill@gmail.com>
2024-03-29 23:41:20 +0700

    update

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,27 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Singleton : MonoBehaviour
-{
-    public static Singleton Instance { get; private set; }
-}
 public class DontDestroyOnLoad : MonoBehaviour
 {
-    public static Singleton Instance { get; private set; }
-
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Instance = this;
-        }
+
         DontDestroyOnLoad(gameObject);
     }
     private void Update()
