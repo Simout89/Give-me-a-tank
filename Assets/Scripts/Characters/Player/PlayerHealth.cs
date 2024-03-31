35ca5f970cd8f7c95e529b7cd4210bbe1ec99cb7
Sim_out <alik2006sootkill@gmail.com>
2024-03-31 12:34:59 +0700

    update

--- Assets/Scripts/Characters/Player/PlayerHealth.cs
+++ Assets/Scripts/Characters/Player/PlayerHealth.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private float HealthMax = 5;
+    [SerializeField] private int MaxLives = 3;
     private float Health;
+    private int Lives;
+    private Vector3 Startpos;
     private void Awake()
     {
+        Startpos = transform.position;
         Health = HealthMax;
+        Lives = MaxLives;
     }
     public void ApplyDamage(float damageValue)
     {
         Health -= damageValue;
         if(Health <= 0)
         {
-            EventManager.OnGameLose.Invoke();
+            Health = HealthMax;
+            Lives--;
+            transform.position = Startpos;
+            if(Lives <= 0)
+            {
+                EventManager.OnGameLose.Invoke();
+            }
         }
     }
 }
